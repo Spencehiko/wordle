b470c471f0f8333c9a8d8c1860f209455a85da53
Spencehiko <hikmet.kufteoglu@gmail.com>
2022-02-25 22:00:16 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <Wordle />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Wordle from './components/Wordle.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        Wordle
+    }
+});
+</script>
+
+<style lang="less">
+html,
+body {
+    padding: 0;
+    margin: 0;
+}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    background: #2c3e50;
+    width: 100%;
+    height: calc(100vh - 60px);
+    padding-top: 60px;
+}
+</style>
---
+++ src/components/Wordle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wordle">
+    {{words}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import store from '@/store';
+import words from '@/datas/words.json';
+import targets from '@/datas/targets.json';
+
+export default defineComponent({
+  name: 'Wordle',
+  data() {
+        return {
+            words: words,
+            targets: targets
+        }
+    },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
